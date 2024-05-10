fdfb773c0108c7b92f8440c3d6fd3d283b8658a0
codemaxi <codemaxi@163.com>
2024-05-10 16:07:49 +0800

    修改后文档一键部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+#######################################
+# 部署前需先将本仓库的修改进行提交
+# 部署需要渠道github.io仓库下操作
+# 详细的操作说明请参考： mkdocs gh-deploy --help
+#######################################
+
 git_user=maxisioux
 
-git remote remove site
-git remote add site https://github.com/${git_user}/${git_user}.github.io.git
+mkdocs_dir=`pwd`
+echo "mkdocs_dir: ${mkdocs_dir}"
+pages_dir=${mkdocs_dir}/../${git_user}.github.io
+pages_dir=$(cd "$(dirname "$pages_dir")"; pwd)/$(basename "$pages_dir")
+echo "pages_dir: ${pages_dir}"
 
 echo "-->start deploy the update docs to github.io"
-mkdocs gh-deploy --config-file mkdocs.yml --remote-name site --remote-branch main
+cd ${pages_dir}
+mkdocs gh-deploy --config-file ${mkdocs}/mkdocs.yml --remote-name origin --remote-branch main
 
+cd ${mkdocs_dir}
 echo "-->deploy finshed, check the site with: https://${git_user}.github.io"
 
